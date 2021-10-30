11d658688a7b6b0afefa589ead850bbeca06930b
andrey <andreiqa@protonmail.com>
2021-10-30 20:48:53 +0300

    дз 3 урок

---
+++ src/main/java/qa/andrei/Cat.java
@@ -0,0 +1,19 @@
+package qa.andrei;
+
+public class Cat {
+        String species;
+        int weight;
+        String color;
+        int age;
+        String breed;
+
+
+        void meow() {
+            System.out.println( "Я " +color+" " +species+" " + breed+ " вешу "+weight+" кг и мне "+age+" года"
+            );
+        }
+
+    }
+
+
+
---
+++ src/main/java/qa/andrei/Main.java
@@ -0,0 +1,16 @@
+package qa.andrei;
+
+public class Main {
+
+        public static void main(String[] args) {
+            Cat my = new Cat();
+            my.color = "черная";
+            my.species = "кошка";
+            my.age = 3;
+            my.breed = "Сибирская";
+            my.weight = 4;
+
+            my.meow();
+        }
+
+}
